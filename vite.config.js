1777d4d131893d14a21ea7e9bb2ae5f2a491fee6
y0015 <285698189@qq.com>
2025-05-19 16:19:57 +0800

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    nodePolyfills({
+      protocolImports: true,
+      crypto: true
+    })
+  ],
+  define: {
+    global: 'globalThis'
+  },
+  optimizeDeps: {
+    include: ['sockjs-client']
+  },
+  server: {
+    host: true,
+    port: 5173
+  }
+})
